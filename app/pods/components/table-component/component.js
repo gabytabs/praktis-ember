e2ef40be63bbc550dccdc5a94e6a2f6cd2a8cfc5
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-14 01:34:30 -0400

    table challenge #2

--- app/pods/components/table-component/component.js
+++ app/pods/components/table-component/component.js
@@ -17,7 +17,6 @@ TableComponentComponent = Ember.Component.extend({
 		return this.results.placements
 	}),
 
-
 	//the observer
 	tableObserver: Ember.observer('placementsData', function() {
 
@@ -28,36 +27,56 @@ TableComponentComponent = Ember.Component.extend({
 		// filtering placementsData
 		for(let i = 0; i < Object.keys(placements).length; i++) {
 
-			let tempArr = {};
+			let tempArr = Em.A();
 
 			if(Object.keys(placements)[i] === models[i].id) {
 
-				let id = Object.keys(placements)[i]
+				let id = Object.keys(placements)[i];
 
 				for(let ii = 0; ii < Object.keys(placements[id]).length; ii++){
-					if(Object.keys(placements[id])[ii] === "count" || Object.keys(placements[id])[ii] === "cost" || Object.keys(placements[id])[ii] === "events" ) {
+					if(Object.keys(placements[id])[ii] === "count" || Object.keys(placements[id])[ii] === "cost") {
+
+						//for the column
 						tempArr[Object.keys(placements[id])[ii]] = placements[id][Object.keys(placements[id])[ii]];
-					}
-				}
-			}
-			this.get('tableColumn').addObject(tempArr)
-		}
 
-		//filtered tablecolumn
-		let filteredColumn = this.get('tableColumn')
+						//for the header
+						this.get('headers').addObject(Object.keys(placements[id])[ii]);
 
-		//retriving all keys on table column
-		filteredColumn.map( headers => {
-			for(let i = 0; i < Object.keys(headers).length; i++) {
-				if(Object.keys(headers)[i] !== "events") {
-					this.get('headers').addObject(Object.keys(headers)[i])
-				} else {
-					for (let ii = 0; ii < Object.keys(headers.events).length; ii++) {
-						this.get('headers').addObject(Object.keys(headers.events)[ii])
+					  // EVENTS
+					} else if (Object.keys(placements[id])[ii] === "events") {
+
+						// headers that are events only
+						let eventsOnly = [];
+						let events = {};
+
+						//For the headers Only
+						for(let e = 0; e < Object.keys(placements[id].events).length; e++) {
+							this.get('headers').addObject(Object.keys(placements[id].events)[e]);
+						};
+
+						let headers = this.get('headers')
+
+						// Just to separate events from count and cost
+						for(let e = 2; e < Object.keys(headers).length; e++){
+							eventsOnly.push(headers[e]);
+						};
+
+						//for events
+						for(let e = 0; e < eventsOnly.length; e++) {
+							if(eventsOnly[e] === Object.keys(placements[id].events)[e]) {
+								events[eventsOnly[e]] = placements[id].events[Object.keys(placements[id].events)[e]];
+							} else {
+								events[eventsOnly[e]] = 0;
+							}
+						}
+						tempArr['events'] = events
 					}
-				}
+				};
 			}
-		});
+			this.get('tableColumn').addObject(tempArr);
+		};
+		console.log(this.get('tableColumn'))
+		console.log(this.get('headers'))
 	})
 });
 
